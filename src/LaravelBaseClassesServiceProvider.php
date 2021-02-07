648fc68bfc1c0571680ccbac8ea105b2803f8f7b
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-07 22:50:46 +0000

    Added service provider

---
+++ src/LaravelBaseClassesServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Thtg88\LaravelBaseClasses;
+
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+use Thtg88\LaravelBaseClasses\Helpers\JournalEntryHelper;
+use Thtg88\LaravelBaseClasses\Validators\CustomValidator;
+
+class LaravelBaseClassesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // Config
+        $this->publishes([
+            __DIR__.'/../config/base-classes.php' => Container::getInstance()
+                ->configPath('base-classes.php'),
+        ], 'base-classes-config');
+
+        // Register custom validator
+        Validator::resolver(static function ($translator, $data, $rules, $messages) {
+            return new CustomValidator($translator, $data, $rules, $messages);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/base-classes.php',
+            'base-classes'
+        );
+
+        // Register journal entry helper singleton
+        $this->app->singleton('JournalEntryHelper', static function ($app) {
+            return $app->make(JournalEntryHelper::class);
+        });
+    }
+}
