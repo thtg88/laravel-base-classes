21a78627ec8dd70165b219fdac0d908a3da46f4b
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-07 21:44:54 +0000

    Added base user model implementation

---
+++ src/Models/User.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Thtg88\LaravelBaseClasses\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'created_at',
+        'deleted_at',
+        'email',
+        'email_verified_at',
+        // 'first_name',
+        // 'last_name',
+        'password',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'deleted_at' => 'datetime',
+        'email_verified_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // MUTATORS
+
+    public function setEmailAttribute($value): void
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
+
+    public function setPasswordAttribute($value): void
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    // RELATIONSHIPS
+
+    public function journal_entries(): MorphMany
+    {
+        return $this->morphMany(JournalEntry::class, 'target');
+    }
+}
