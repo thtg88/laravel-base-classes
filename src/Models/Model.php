427fdaab0ed5f3acf640cf7ad81ec23679d7738f
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-07 21:42:42 +0000

    Make base model class abstract

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Model extends BaseModel
+abstract class Model extends BaseModel
 {
     use HasFactory, SoftDeletes;
 
