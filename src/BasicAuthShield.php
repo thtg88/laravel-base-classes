a65d51bd36be527376fbef9e0f9e79cca1d095fe
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-07 21:38:06 +0000

    Added basic auth layer

---
+++ src/BasicAuthShield.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Thtg88\LaravelBaseClasses;
+
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Vinkla\Shield\Shield;
+
+class BasicAuthShield extends Shield
+{
+    /**
+     * Create a new shield instance.
+     *
+     * @param array $users
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->users = config('shield.users');
+    }
+
+    /**
+     * Verify the user input.
+     *
+     * @param string|null $username
+     * @param string|null $password
+     * @param string|null $user
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     * @return bool
+     */
+    public function verify(
+        ?string $username,
+        ?string $password,
+        ?string $user = null
+    ): bool {
+        if ($username === null || $password === null) {
+            return false;
+        }
+
+        $users = $this->getUsers($user);
+
+        foreach ($users as $user => $credentials) {
+            if (
+                $username === reset($credentials) &&
+                $password === end($credentials)
+            ) {
+                $this->currentUser = $user;
+
+                return true;
+            }
+        }
+
+        throw new UnauthorizedHttpException('Basic');
+    }
+}
---
+++ src/Http/Middleware/BasicAuthMiddleware.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Thtg88\LaravelBaseClasses\Http\Middleware;
+
+use Thtg88\LaravelBaseClasses\BasicAuthShield;
+use Closure;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class BasicAuthMiddleware
+{
+    /**
+     * The shield instance.
+     *
+     * @var \Thtg88\LaravelBaseClasses\BasicAuthShield
+     */
+    protected BasicAuthShield $shield;
+
+    /**
+     * Create a new middleware class.
+     *
+     * @param \Thtg88\LaravelBaseClasses\BasicAuthShield $shield
+     * @return void
+     */
+    public function __construct(BasicAuthShield $shield)
+    {
+        $this->shield = $shield;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null $user
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ?string $user = null)
+    {
+        // Don't shield if disabled
+        if (config('shield.mode') === false) {
+            return $next($request);
+        }
+
+        $verified = $this->shield->verify(
+            $request->getUser(),
+            $request->getPassword(),
+            $user
+        );
+        if ($verified === false) {
+            throw new UnauthorizedHttpException('Basic');
+        }
+
+        return $next($request);
+    }
+}
