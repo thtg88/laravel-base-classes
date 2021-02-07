0b5dbca126cedfa74f2db9ac263eac2210552ee5
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-07 21:44:00 +0000

    Journal entry model now extends base model

--- src/Models/JournalEntry.php
+++ src/Models/JournalEntry.php
@@ -2,7 +2,6 @@
 
 namespace Thtg88\LaravelBaseClasses\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
